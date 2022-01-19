cb65fd85cb118cf71f7889d1966f20fab31df953
cristi <cristi@kth.se>
2022-01-19 21:42:30 +0100

    removed uselless returns

--- test/tw1.1-10.dishType.test.js
+++ test/tw1.1-10.dishType.test.js
@@ -22,12 +22,12 @@ describe("TW1.1 dishType", function() {
         assert.equal(isKnownTypeCB("appetizer"), false);
     });
     
-    it("dishType returns a known dish type", function(){ return assert.equal(dishType(dishesConst[4]), "main course");});
+    it("dishType returns a known dish type", function(){ assert.equal(dishType(dishesConst[4]), "main course");});
     //    it("should recognize starter in first position", function(){ return assert.equal(dishType(dishesConst[1]), "starter");});
     //    it("should recognize starter in other position", function(){ return assert.equal(dishType(dishesConst[2]), "starter");});
     //    it("should recognize dessert in first position", function(){ return assert.equal(dishType(dishesConst[6]), "dessert");});
     //    it("should recognize dessert in other position", function(){ return assert.equal(dishType(dishesConst[5]), "dessert");});
-    it("dishType returns empty string if starter, main course, dessert not present", function(){ return assert.equal(dishType(dishesConst[0]), "");});
-    it("dishType returns empty string if dishTypes property not present", function(){ return assert.equal(dishType(dishesConst[7]), "");});
+    it("dishType returns empty string if starter, main course, dessert not present", function(){ assert.equal(dishType(dishesConst[0]), "");});
+    it("dishType returns empty string if dishTypes property not present", function(){ assert.equal(dishType(dishesConst[7]), "");});
 });
 
