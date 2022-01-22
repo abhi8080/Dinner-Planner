137a0eacdd30142be542788c48b4574802d2f838
cristi <cristi@kth.se>
2022-01-22 11:26:16 +0100

    fixed test errors if cloning was done after sorting... Cloning is testet first, then sorting

--- test/tw1.1-20.dishSort.test.js
+++ test/tw1.1-20.dishSort.test.js
@@ -5,20 +5,24 @@ const {sortDishes}= require('../src/'+TEST_PREFIX+'utilities.js');
 describe("TW1.1 sortDishes", function() {
     this.timeout(200000);  // increase to allow debugging during the test run
     
-    it("sort order should be: 'no type', starter, main course, dessert", function(){
-        const sorted= sortDishes(
-            [dishesConst[4], dishesConst[6], dishesConst[2], dishesConst[7]]
-        );
-        assert.equal(sorted.length, 4);
-        assert.equal(sorted[0], dishesConst[7]);
-        assert.equal(sorted[1], dishesConst[2]);
-        assert.equal(sorted[2], dishesConst[4]);
-        assert.equal(sorted[3], dishesConst[6]);
-    });
     it("sorted array should not be the same object as original array. Use e.g. spread syntax [...array]", function(){
+        const array= [dishesConst[4], dishesConst[6], dishesConst[2], dishesConst[7]];
+        const arrayCopy=[...array];
+        const sorted= sortDishes(array);
+        expect(sorted).to.not.equal(array);
+        array.forEach(function checkDishCB(dish, index){
+            expect(dish).to.equal(
+                arrayCopy[index],
+                "do not sort the original array, copy/spread the array, then sort the copy");
+        });
+    });
+    it("sort order should be: 'no type', starter, main course, dessert", function(){
         const array= [dishesConst[4], dishesConst[6], dishesConst[2], dishesConst[7]];
         const sorted= sortDishes(array);
         assert.equal(sorted.length, 4);
-        expect(sorted).to.not.equal(array);
+        assert.equal(sorted[0], array[3]);
+        assert.equal(sorted[1], array[2]);
+        assert.equal(sorted[2], array[0]);
+        assert.equal(sorted[3], array[1]);
     });
 });
--- test/tw1.1-30.ingredients.test.js
+++ test/tw1.1-30.ingredients.test.js
@@ -11,21 +11,25 @@ const ingredientsConst= [
 describe("TW1.1 sortIngredients", function() {
     this.timeout(200000);  // increase to allow debugging during the test run
     
-    it("should sort by aisle first, then by name", function(){
-        const ingredients= [...ingredientsConst];
-        const sorted= sortIngredients(ingredients);
-        
-        assert.equal(sorted.length, ingredients.length) ;
-        assert.equal(sorted[0], ingredients[3]) ;
-        assert.equal(sorted[1], ingredients[1]);
-        assert.equal(sorted[2], ingredients[2]);
-        assert.equal(sorted[3], ingredients[0]);
-    });
     it("sorted array should not be the same object as original array. Use e.g. spread syntax [...array]", function(){
         const ingredients= [...ingredientsConst];
         const sorted= sortIngredients(ingredients);
 
         assert.equal(sorted.length, ingredients.length);
         expect(sorted).to.not.equal(ingredients);
+        ingredients.forEach(function checkIngrCB(i, index){
+            expect(i).to.equal(
+                ingredientsConst[index],
+                "do not sort the original array, copy/spread the array, then sort the copy");
+        });
+    });
+    it("should sort by aisle first, then by name", function(){
+        const ingredients= [...ingredientsConst];
+        const sorted= sortIngredients(ingredients);
+        assert.equal(sorted.length, ingredients.length) ;
+        assert.equal(sorted[0], ingredients[3]) ;
+        assert.equal(sorted[1], ingredients[1]);
+        assert.equal(sorted[2], ingredients[2]);
+        assert.equal(sorted[3], ingredients[0]);
     });
 });
