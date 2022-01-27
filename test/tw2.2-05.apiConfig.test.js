4d2db7780ac78590adb46719d0551f9604e76f25
cristi <cristi@kth.se>
2022-01-27 19:13:37 +0100

    made the test robust

--- test/tw2.2-05.apiConfig.test.js
+++ test/tw2.2-05.apiConfig.test.js
@@ -6,17 +6,15 @@ try {
   apiConfig = require('../src/' + X + 'apiConfig.js');
 } catch (e) {}
 
-const { BASE_URL, API_KEY } = apiConfig;
-
 describe('TW2.2 apiConfig', function () {
   before(function () {
     if (!apiConfig) this.skip();
   });
 
   it('apiConfig exports BASE_URL and API_KEY', function () {
-    expect(BASE_URL).to.not.be.undefined;
-    expect(API_KEY).to.not.be.undefined;
-    expect(BASE_URL).to.be.a('string');
-    expect(API_KEY).to.be.a('string');
+    expect(apiConfig.BASE_URL).to.not.be.undefined;
+    expect(apiConfig.API_KEY).to.not.be.undefined;
+    expect(apiConfig.BASE_URL).to.be.a('string');
+    expect(apiConfig.API_KEY).to.be.a('string');
   });
 });
