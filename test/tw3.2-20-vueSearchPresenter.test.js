73f7082c322afda0a45ad7bae4054890244db169
cristi <cristi@kth.se>
2022-02-14 13:17:25 +0100

    presenter tests with mock model and mock view

---
+++ test/tw3.2-20-vueSearchPresenter.test.js
@@ -0,0 +1,196 @@
+import { assert, expect } from "chai";
+import installOwnCreateElement from "./jsxCreateElement";
+import {h, render} from "vue";
+import {withMyFetch, mySearchFetch, findCGIParam, searchResults} from "./mockFetch.js";
+import {findTag, prepareViewWithCustomEvents} from "./jsxUtilities.js";
+
+
+let SearchPresenter;
+let SearchFormView;
+let SearchResultsView;
+const X = TEST_PREFIX;
+
+try {
+    SearchPresenter = require("../src/vuejs/" + X + "searchPresenter.js").default;
+    SearchFormView = require("../src/views/" + X + "searchFormView.js").default;
+    SearchResultsView = require("../src/views/" + X + "searchResultsView.js").default;
+} catch (e) {console.log(e);}
+
+
+function findFormEventNames(){
+    const {customEventNames}= prepareViewWithCustomEvents(
+        SearchFormView,
+        {dishTypeOptions:['starter', 'main course', 'dessert']},
+        function collectControls(rendering){
+            const buttons=findTag("button", rendering).filter(function(button){ return button.children.flat()[0].toLowerCase().trim().startsWith("search"); });
+            const selects=findTag("select", rendering);
+            const inputs=findTag("input", rendering);
+            expect(buttons.length, "SearchFormview expected to have one search button").to.equal(1);
+            expect(inputs.length, "SearchFormView expected to have one  input box").to.equal(1);
+            expect(selects.length, "SearchFormView expected to have one  select box").to.equal(1);
+            return [...inputs, ...selects, ...buttons];
+        });
+    return customEventNames;
+}
+
+//var nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value").set;
+//nativeInputValueSetter.call(input, 'react 16 value');
+
+function findResultsEventName(){
+    const {customEventNames}= prepareViewWithCustomEvents(
+        SearchResultsView,
+        {searchResults},
+        function findSpanes(rendering){
+            return findTag("span", rendering).filter(function(span){ return span.props.onClick; });
+        });
+    return customEventNames;
+}
+
+
+describe("TW3.2 Vue stateful Search presenter", function () {
+    this.timeout(200000);
+
+    const formProps=[];
+    const resultsProps=[];
+    function DummyForm(props){
+        formProps.push(props);
+        return <span>dummy form</span>;
+    }
+    function DummyResults(props){
+        resultsProps.push(props);
+        return <span>dummy results</span>;
+    }
+
+    function replaceViews(tag, props, ...children){
+        if(tag==SearchFormView)
+            return h(DummyForm, props, ...children);
+        if(tag==SearchResultsView)
+            return h(DummyResults, props, ...children);
+        return h(tag, props, ...children);
+    };
+    
+    let currentDishId;
+
+    async function doRender(){
+        const div= document.createElement("div");
+        window.React={createElement: replaceViews};
+        formProps.length=0;
+        resultsProps.length=0;
+        
+        await withMyFetch(
+            mySearchFetch,
+            function theRender(){
+                render(<SearchPresenter model={{
+                    setCurrentDish(id){
+                        currentDishId=id;
+                    }
+                }}/>, div);
+            },
+            function makeResults(url){
+                return {results:searchResults};
+            }  
+        );
+        return div;
+    }
+    before(async function () {
+        if (!SearchPresenter) this.skip();
+    });
+    after(function(){
+        React.createElement=h;
+    });
+
+    it("Search presenter initiates a search promise at first render and resolves the promise in component state", async function(){
+        const [resultChosen]= findResultsEventName();
+        const div= await doRender();
+        expect(JSON.stringify(formProps.slice(-1)[0]["dishTypeOptions"]), "the options passed are not correct").to.equal(
+            JSON.stringify(["starter", "main course", "dessert"])
+        );
+
+        expect(mySearchFetch.lastFetch, "presenter should launch a search at component creation").to.be.ok;
+        expect(findCGIParam(mySearchFetch.lastFetch, "type", ""), "first search launched by presenter should be with empty params").to.be.ok;
+        expect(findCGIParam(mySearchFetch.lastFetch, "query", ""), "first search launched by presenter should be with empty params").to.be.ok;
+        
+        expect(div.firstElementChild.firstElementChild.nextSibling.tagName, "an image expected to be rendered during promise resolve").to.equal("IMG");
+        expect(resultsProps.length, "search results view should not be rendered during promise resolve").to.equal(0);
+
+        await new Promise(resolve => setTimeout(resolve)); // wait for eventual promise to resolve
+
+        expect(resultsProps.length, "search results view should be rendered after promise resolve").to.equal(1);
+        expect(JSON.stringify(resultsProps[0].searchResults), "search results view should be rendered after promise resolve").to.equal(JSON.stringify(searchResults));
+
+        expect(div.firstElementChild.firstElementChild.nextSibling.tagName, "the search results view expected to be rendered after promise resolve").to.equal("SPAN");
+        expect(div.firstElementChild.firstElementChild.nextSibling.textContent, "the search results view expected to be rendered after promise resolve").to.equal("dummy results");
+
+        resultsProps.slice(-1)[0][resultChosen]({id:42});
+        expect(currentDishId, "clicking on a search results should set the current dish in the model").to.equal(42);
+    });
+    
+    it("Search presenter initiates a search promise after filling the form and button click", async function(){
+        const [setText, setType, doSearch]= findFormEventNames();
+        const [resultChosen]= findResultsEventName();
+
+        const div= await doRender();
+        await new Promise(resolve => setTimeout(resolve)); // wait for eventual promise to resolve
+        
+        mySearchFetch.lastFetch=undefined;
+        resultsProps.length=0;
+        await withMyFetch(   // just in case we have "search as you type";
+            mySearchFetch,
+            async function interact(){
+                formProps.slice(-1)[0][setType]("main course");
+                formProps.slice(-1)[0][setText]("pizza");
+                formProps.slice(-1)[0][doSearch]();
+            },
+            function makeResults(url){
+                return {results:[searchResults[1], searchResults[0]]};
+            }            
+        );
+        expect(mySearchFetch.lastFetch, "presenter should launch a search at button click").to.be.ok;
+        expect(findCGIParam(mySearchFetch.lastFetch, "type", "main course"), "search should use type parameter from state").to.be.ok;
+        expect(findCGIParam(mySearchFetch.lastFetch, "query", "pizza"), "search should use text parameter from state").to.be.ok;
+        
+        expect(div.firstElementChild.firstElementChild.nextSibling.tagName, "an image expected to be rendered during promise resolve").to.equal("IMG");
+        expect(resultsProps.length, "search results view should not be rendered during promise resolve").to.equal(0);
+
+        await new Promise(resolve => setTimeout(resolve));
+
+        expect(resultsProps.length, "search results view should be rendered after promise resolve").to.equal(1);
+        expect(JSON.stringify(resultsProps.slice(-1)[0].searchResults), "search results view should be rendered after promise resolve").to.equal(JSON.stringify([searchResults[1], searchResults[0]]));
+        expect(div.firstElementChild.firstElementChild.nextSibling.tagName, "the search results view expected to be rendered after promise resolve").to.equal("SPAN");
+        expect(div.firstElementChild.firstElementChild.nextSibling.textContent, "the search results view expected to be rendered after promise resolve").to.equal("dummy results");
+
+        resultsProps.slice(-1)[0][resultChosen]({id:43});
+        expect(currentDishId, "clicking on a search results should set the current dish in the model").to.equal(43);
+
+    });
+    
+    it("on successive searches, presenter only renders results of last search", async function(){
+        const [setText, setType, doSearch]= findFormEventNames();
+        
+        const div= await doRender();
+        await new Promise(resolve => setTimeout(resolve)); // wait for initial promise to resolve
+        
+        mySearchFetch.lastFetch=undefined;
+        const formLen= formProps.length;
+        let formLen2;
+        await withMyFetch(
+            mySearchFetch,
+            async function interact(){
+                formProps.slice(-1)[0][setType]("dessert");
+                formProps.slice(-1)[0][doSearch]();
+                formProps.slice(-1)[0][setType]("starter");
+                formProps.slice(-1)[0][doSearch]();
+            },
+            function makeResults(url){
+                if(url.indexOf("dessert")!=-1)
+                    return {results:[searchResults[1], searchResults[0]], delay:3};
+                else
+                    return {results:[searchResults[1]]};
+            }
+        );
+
+        expect(resultsProps.find(p=>p.searchResults.length==2)).to.not.be.ok;
+        await new Promise(resolve => setTimeout(resolve, 5));
+        expect(resultsProps.find(p=>p.searchResults.length==2)).to.not.be.ok;
+ });
+});
