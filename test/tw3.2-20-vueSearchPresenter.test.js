f4d0dbcecb4167dab62177841887a69e65c44d78
cristi <cristi@kth.se>
2022-02-14 23:34:02 +0100

    added phases for a graceful transition to component state

--- test/tw3.2-20-vueSearchPresenter.test.js
+++ test/tw3.2-20-vueSearchPresenter.test.js
@@ -3,6 +3,7 @@ import installOwnCreateElement from "./jsxCreateElement";
 import {h, render} from "vue";
 import {withMyFetch, mySearchFetch, findCGIParam, searchResults} from "./mockFetch.js";
 import {findTag, prepareViewWithCustomEvents} from "./jsxUtilities.js";
+import {searchDishes} from "/src/solved-dishSource.js";
 
 
 let SearchPresenter;
@@ -70,7 +71,37 @@ describe("TW3.2 Vue stateful Search presenter", function () {
     };
     
     let currentDishId;
+    const oldParams={};
+    function makeModel(){
+        return {
+            setCurrentDish(id){
+                currentDishId=id;
+            },
+            // temporary for the first test only!
+            doSearch(params){
+                this.searchResultsPromiseState.promise=searchDishes(params).then(results=>this.searchResultsPromiseState.data=results);
+                this.searchResultsPromiseState.data=null;
+            },
+            // temporary for first test only!
+            searchResultsPromiseState:{},
+            // temporary
+            setSearchQuery(query){ this.searchParams.query=query; },
+            setSearchType(type){ this.searchParams.type=type; },
+            // temporary
+            searchParams:{},
+        };
+    };
 
+    let vueModel;
+    const Root={
+        data(){ return {rootModel:makeModel()};},
+        render(){
+            return <SearchPresenter model={this.rootModel}/>;
+        },
+        created(){
+            vueModel= this.rootModel;
+        }
+    };
     async function doRender(){
         const div= document.createElement("div");
         window.React={createElement: replaceViews};
@@ -80,11 +111,7 @@ describe("TW3.2 Vue stateful Search presenter", function () {
         await withMyFetch(
             mySearchFetch,
             function theRender(){
-                render(<SearchPresenter model={{
-                    setCurrentDish(id){
-                        currentDishId=id;
-                    }
-                }}/>, div);
+                render(<Root/>, div);
             },
             function makeResults(url){
                 return {results:searchResults};
@@ -99,34 +126,106 @@ describe("TW3.2 Vue stateful Search presenter", function () {
         React.createElement=h;
     });
 
-    it("Search presenter initiates a search promise at first render and resolves the promise in component state", async function(){
-        const [resultChosen]= findResultsEventName();
+    
+    it("Search presenter initial test, should also pass with TW2 functional code", async function(){
+        const [setText, setType, doSearch]= findFormEventNames();
         const div= await doRender();
-        expect(JSON.stringify(formProps.slice(-1)[0]["dishTypeOptions"]), "the options passed are not correct").to.equal(
-            JSON.stringify(["starter", "main course", "dessert"])
+        await new Promise(resolve => setTimeout(resolve, 10));
+
+        expect(formProps.slice(-1)[0][setType]).to.be.a("Function");
+        expect(formProps.slice(-1)[0][setText]).to.be.a("Function");
+        expect(formProps.slice(-1)[0][doSearch]).to.be.a("Function");
+
+        mySearchFetch.lastFetch=undefined;
+        resultsProps.length=0;
+        await withMyFetch(  
+            mySearchFetch,
+            async function interact(){
+                formProps.slice(-1)[0][setType]("main course");
+                formProps.slice(-1)[0][setText]("calzone");
+                formProps.slice(-1)[0][doSearch]();
+            },
+            function makeResults(url){
+                return {results:[searchResults[1], searchResults[0]]};
+            }            
         );
 
-        expect(mySearchFetch.lastFetch, "presenter should launch a search at component creation").to.be.ok;
-        expect(findCGIParam(mySearchFetch.lastFetch, "type", ""), "first search launched by presenter should be with empty params").to.be.ok;
-        expect(findCGIParam(mySearchFetch.lastFetch, "query", ""), "first search launched by presenter should be with empty params").to.be.ok;
+        expect(mySearchFetch.lastFetch, "presenter should initiate a search at button click").to.be.ok;
+        expect(findCGIParam(mySearchFetch.lastFetch, "type", "main course"), "search should use type parameter from state").to.be.ok;
+        expect(findCGIParam(mySearchFetch.lastFetch, "query", "calzone"), "search should use text parameter from state").to.be.ok;
         
         expect(div.firstElementChild.firstElementChild.nextSibling.tagName, "an image expected to be rendered during promise resolve").to.equal("IMG");
         expect(resultsProps.length, "search results view should not be rendered during promise resolve").to.equal(0);
-
-        await new Promise(resolve => setTimeout(resolve)); // wait for eventual promise to resolve
-
+       
+        await new Promise(resolve => setTimeout(resolve));
+        
         expect(resultsProps.length, "search results view should be rendered after promise resolve").to.equal(1);
-        expect(JSON.stringify(resultsProps[0].searchResults), "search results view should be rendered after promise resolve").to.equal(JSON.stringify(searchResults));
-
+        expect(JSON.stringify(resultsProps.slice(-1)[0].searchResults), "search results view should be rendered after promise resolve").to.equal(JSON.stringify([searchResults[1], searchResults[0]]));
         expect(div.firstElementChild.firstElementChild.nextSibling.tagName, "the search results view expected to be rendered after promise resolve").to.equal("SPAN");
         expect(div.firstElementChild.firstElementChild.nextSibling.textContent, "the search results view expected to be rendered after promise resolve").to.equal("dummy results");
-
-        expect(resultsProps.slice(-1)[0][resultChosen]).to.be.a("Function");
-        resultsProps.slice(-1)[0][resultChosen]({id:42});
-        expect(currentDishId, "clicking on a search results should set the current dish in the model").to.equal(42);
     });
-    
-    it("Search presenter initiates a search promise after filling the form and button click", async function(){
+
+
+    it("Search presenter object component test, convert your functional component to an object!", async function(){
+        expect(SearchPresenter, "presenter must now be an object so we can add state").to.be.a("Object");
+        const [setText, setType, doSearch]= findFormEventNames();
+        const div= await doRender();
+        await new Promise(resolve => setTimeout(resolve, 10));
+
+        expect(formProps.slice(-1)[0][setType]).to.be.a("Function");
+        expect(formProps.slice(-1)[0][setText]).to.be.a("Function");
+        expect(formProps.slice(-1)[0][doSearch]).to.be.a("Function");
+
+        mySearchFetch.lastFetch=undefined;
+        resultsProps.length=0;
+        await withMyFetch(  
+            mySearchFetch,
+            async function interact(){
+                formProps.slice(-1)[0][setType]("main course");
+                formProps.slice(-1)[0][setText]("calzone");
+                formProps.slice(-1)[0][doSearch]();
+            },
+            function makeResults(url){
+                return {results:[searchResults[1], searchResults[0]]};
+            }            
+        );
+
+        expect(mySearchFetch.lastFetch, "presenter should initiate a search at button click").to.be.ok;
+        expect(findCGIParam(mySearchFetch.lastFetch, "type", "main course"), "search should use type parameter from state").to.be.ok;
+        expect(findCGIParam(mySearchFetch.lastFetch, "query", "calzone"), "search should use text parameter from state").to.be.ok;
+        
+        expect(div.firstElementChild.firstElementChild.nextSibling.tagName, "an image expected to be rendered during promise resolve").to.equal("IMG");
+        expect(resultsProps.length, "search results view should not be rendered during promise resolve").to.equal(0);
+       
+        await new Promise(resolve => setTimeout(resolve));
+        
+        expect(resultsProps.length, "search results view should be rendered after promise resolve").to.equal(1);
+        expect(JSON.stringify(resultsProps.slice(-1)[0].searchResults), "search results view should be rendered after promise resolve").to.equal(JSON.stringify([searchResults[1], searchResults[0]]));
+        expect(div.firstElementChild.firstElementChild.nextSibling.tagName, "the search results view expected to be rendered after promise resolve").to.equal("SPAN");
+        expect(div.firstElementChild.firstElementChild.nextSibling.textContent, "the search results view expected to be rendered after promise resolve").to.equal("dummy results");
+    });
+
+    it("Search presenter object component stores search parameters in component state", async function(){
+        expect(SearchPresenter, "presenter must now be an object so we can add state").to.be.a("Object");
+        const [setText, setType, doSearch]= findFormEventNames();
+        const div= await doRender();
+        await new Promise(resolve => setTimeout(resolve, 10));
+        
+        expect(formProps.slice(-1)[0][setType]).to.be.a("Function");
+        expect(formProps.slice(-1)[0][setText]).to.be.a("Function");
+        expect(formProps.slice(-1)[0][doSearch]).to.be.a("Function");
+        
+        mySearchFetch.lastFetch=undefined;
+        resultsProps.length=0;
+        
+        formProps.slice(-1)[0][setType]("main course");
+        formProps.slice(-1)[0][setText]("calzone");
+
+        expect(vueModel.searchParams, "You should not store search params in application state any longer").to.be.empty;
+        expect(SearchPresenter.created, "use created() to initiate the first search promise").to.be.a("Function");
+    });
+
+    it("Search presenter resolves the promise in component state after filling the form and button click", async function(){
         const [setText, setType, doSearch]= findFormEventNames();
         const [resultChosen]= findResultsEventName();
 
@@ -138,7 +237,7 @@ describe("TW3.2 Vue stateful Search presenter", function () {
         
         mySearchFetch.lastFetch=undefined;
         resultsProps.length=0;
-        await withMyFetch(   // just in case we have "search as you type";
+        await withMyFetch(   
             mySearchFetch,
             async function interact(){
                 formProps.slice(-1)[0][setType]("main course");
@@ -149,6 +248,9 @@ describe("TW3.2 Vue stateful Search presenter", function () {
                 return {results:[searchResults[1], searchResults[0]]};
             }            
         );
+        expect(vueModel.searchParams, "You should not store search params in application state any longer").to.be.empty;
+        expect(vueModel.searchResultsPromiseState, "You should not resolve the promise in application state any longer").to.be.empty;
+        
         expect(mySearchFetch.lastFetch, "presenter should launch a search at button click").to.be.ok;
         expect(findCGIParam(mySearchFetch.lastFetch, "type", "main course"), "search should use type parameter from state").to.be.ok;
         expect(findCGIParam(mySearchFetch.lastFetch, "query", "pizza"), "search should use text parameter from state").to.be.ok;
@@ -166,7 +268,43 @@ describe("TW3.2 Vue stateful Search presenter", function () {
         resultsProps.slice(-1)[0][resultChosen]({id:43});
         expect(currentDishId, "clicking on a search results should set the current dish in the model").to.equal(43);
 
+        expect(SearchPresenter.created, "use created() to initiate the first search promise").to.be.a("Function");
     });
+
+    it("Search presenter initiates a search promise at first render and resolves the promise in component state", async function(){
+        const [resultChosen]= findResultsEventName();
+        expect(SearchPresenter.created, "use created() to initiate the first search promise").to.be.a("Function");
+        mySearchFetch.lastFetch=undefined;
+        resultsProps.length=0;
+        const div= await doRender();
+        
+        expect(JSON.stringify(formProps.slice(-1)[0]["dishTypeOptions"]), "the options passed are not correct").to.equal(
+            JSON.stringify(["starter", "main course", "dessert"])
+        );
+
+        expect(vueModel.searchParams, "You should not store search params in application state any longer").to.be.empty;
+        expect(vueModel.searchResultsPromiseState, "You should not resolve the promise in application state any longer").to.be.empty;
+        
+        expect(mySearchFetch.lastFetch, "presenter should initiate a search at component creation").to.be.ok;
+        expect(findCGIParam(mySearchFetch.lastFetch, "type", ""), "first search launched by presenter should be with empty params").to.be.ok;
+        expect(findCGIParam(mySearchFetch.lastFetch, "query", ""), "first search launched by presenter should be with empty params").to.be.ok;
+        
+        expect(div.firstElementChild.firstElementChild.nextSibling.tagName, "an image expected to be rendered during promise resolve").to.equal("IMG");
+        expect(resultsProps.length, "search results view should not be rendered during promise resolve").to.equal(0);
+
+        await new Promise(resolve => setTimeout(resolve)); // give the UI a chance to update
+
+        expect(resultsProps.length, "search results view should be rendered after promise resolve").to.equal(1);
+        expect(JSON.stringify(resultsProps[0].searchResults), "search results view should be rendered after promise resolve").to.equal(JSON.stringify(searchResults));
+
+        expect(div.firstElementChild.firstElementChild.nextSibling.tagName, "the search results view expected to be rendered after promise resolve").to.equal("SPAN");
+        expect(div.firstElementChild.firstElementChild.nextSibling.textContent, "the search results view expected to be rendered after promise resolve").to.equal("dummy results");
+
+        expect(resultsProps.slice(-1)[0][resultChosen]).to.be.a("Function");
+        resultsProps.slice(-1)[0][resultChosen]({id:42});
+        expect(currentDishId, "clicking on a search results should set the current dish in the model").to.equal(42);
+    });
+    
     
     it("on successive searches, presenter only renders results of last search", async function(){
         const [setText, setType, doSearch]= findFormEventNames();
