0ac5169352d09a844f41e86d6ea0ff3e497be0de
cristi <cristi@kth.se>
2022-10-16 02:42:51 +0200

    fixes #64

--- test/tw2.2-10.getDishDetails.test.js
+++ test/tw2.2-10.getDishDetails.test.js
@@ -1,4 +1,5 @@
 const { assert, expect } = require("chai");
+import {withMyFetch, checkFetchUrl, myDetailsFetch} from "./mockFetch.js";
 
 let getDishDetails;
 const X = TEST_PREFIX;
@@ -8,6 +9,7 @@ try {
   else getDishDetails = dishSource.default.getDishDetails;
 } catch (e) {console.log(e);}
 
+
 describe("TW2.2 getDishDetails", function tw2_2_10() {
   this.timeout(200000);
 
@@ -15,49 +17,61 @@ describe("TW2.2 getDishDetails", function tw2_2_10() {
     if (!getDishDetails) this.skip();
   });
 
-  function testPromise(text, p, expectedDish) {
-      it(text, async function tw2_2_10_testPromise(){
-      let start = new Date();
-      let dish1 = await p();
-      let finish = new Date();
-      expect(
-        finish - start,
-        "promise getDishDetails should take minimum 2 ms"
-      ).to.be.above(2);
-      expect(
-        dish1.id,
-        "Dish id" + dish1.id + "not equal to expected dish id" + expectedDish.id
-      ).to.equal(expectedDish.id);
-      expect(
-        dish1.title,
-        "Dish title" +
-          dish1.title +
-          "not equal to expected dish title" +
-          expectedDish.title
-      ).to.equal(expectedDish.title);
-    }).timeout(4000);
-  }
-
-    testPromise("getDishDetails promise #1",  function tw2_2_10_testPromise1(){return getDishDetails(547775);}, {
-        id: 547775,
-        title: "Creamy Avocado Pasta",
+    function testPromise(text, p, expectedIdAndHash) {
+        it(text, async function tw2_2_10_testPromise(){
+            const dish= await withMyFetch(myDetailsFetch, p);
+            expect(dish, "getDishDetails expected to return Promise").to.be.ok;
+            expect(dish.id, "getDishDetails expected to return a dish with the given id").to.equal(expectedIdAndHash.id);
+            checkFetchUrl(myDetailsFetch.lastFetch, myDetailsFetch.lastParam, expectedIdAndHash.hash);
+        }).timeout(4000);
+    }
+    
+    testPromise("getDishDetails promise #1",  function tw2_2_10_testPromise1(){return getDishDetails(1445969);}, {
+        id: 1445969,
+        hash:  -1407095300
     });
-
+    
+    
     testPromise("getDishDetails promise #2", function tw2_2_10_testPromise2(){return  getDishDetails(601651);}, {
         id: 601651,
-        title: "Fruit Pizza",
+        hash: 105207665
     });
-
+    
     it("getDishDetails promise must reject if the dish with the given ID does not exist", async function tw2_2_10_3(){
         let error;
-        try {
-            error= await new Promise(function(resolve, reject){
-                return getDishDetails(undefined).then(reject, resolve);
-            });
-        } catch (e) {
-            assert.fail("the promise did not reject");
-    }
-        expect(error, "getDishDetails(bad_param) must reject with a truthy error").to.be.ok;
-
-  }).timeout(4000);
+        let fetchErr;
+        try{
+            await withMyFetch(function myFetch(url, param){
+                try{
+                    checkFetchUrl(url, param, 1439408948);
+                }catch(e){
+                    fetchErr=e;
+                }
+                if(!fetchErr)
+                    return  Promise.resolve({
+                        ok:false,
+                        status:404,
+                        json(){
+                            return Promise.resolve({ msg: "dummy error dish not found"});
+                        }
+                    });
+                else return Promise.resolve({
+                    ok:true,
+                    status:200,
+                    json(){
+                        return Promise.resolve({ id:-1});
+                    }
+                });
+            },
+                              function returnPromise(){
+                                  return getDishDetails(undefined);
+                              }
+                             );
+        }catch (e) {
+            error=e;
+        }
+        if(fetchErr)
+            throw fetchErr;
+        expect(error, "getDishDetails(bad_param) must reject").to.be.ok;
+    }).timeout(4000);
 });
