5239dcf8c23e4f0b9d604a412f4bc9ebe2018333
cristi <cristi@kth.se>
2022-10-19 13:07:12 +0200

    fixed small bug on #9

--- test/tw1.1-07-sortIngredients.test.js
+++ test/tw1.1-07-sortIngredients.test.js
@@ -20,7 +20,7 @@ describe("TW1.1 sortIngredients", function tw1_1_30() {
     it("compareIngredientsCB compares by aisle, then by name", function tw1_1_30_0(){
         const {compareIngredientsCB}= utilities;
 
-        expect(compareIngredientsCB(ingredientsConst[0], ingredientsConst[1]), "if first aisle is bigger than second aisle, reverse order (return a positive number)").to.be.lt(0);
+        expect(compareIngredientsCB(ingredientsConst[0], ingredientsConst[1]), "if first aisle is bigger than second aisle, reverse order (return a positive number)").to.be.gt(0);
         expect(compareIngredientsCB(ingredientsConst[1], ingredientsConst[0]), "if first aisle is smaller than second aisle, do not reverse order (return a negative number)").to.be.lt(0);
         expect(compareIngredientsCB(ingredientsConst[0], ingredientsConst[2]), "if aisle is the same and first name is bigger than second name, reverse order (return a positive number)").to.be.gt(0);
         expect(compareIngredientsCB(ingredientsConst[2], ingredientsConst[0]), "if aisle is the same and first name is smaller than second name, do not reverse order (return a negative number)").to.be.lt(0);
