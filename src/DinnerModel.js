385a06e3c8f7021da5bccd8014a52b539d019399
Abhinav Kalra <kalra@kth.se>
2022-11-09 09:53:42 +0100

    TW3.1 almost completed

--- src/DinnerModel.js
+++ src/DinnerModel.js
@@ -8,6 +8,7 @@ import {getDishDetails, searchDishes} from './dishSource.js'
 */
 class DinnerModel {
   constructor(nrGuests = 2, dishArray = [], currentDish) {
+    this.observers = [];
     this.setNumberOfGuests(nrGuests);
     this.dishes = dishArray;
     this.searchResultsPromiseState = {};
@@ -20,7 +21,10 @@ class DinnerModel {
     if (nr < 1 || !Number.isInteger(nr))
       throw "number of guests not a positive integer";
 
-    this.numberOfGuests = nr;
+    if(nr !== this.numberOfGuests) {
+      this.numberOfGuests = nr;
+      this.notifyObservers({nrOfGuests: nr})
+    }
     // the error message must be exactly "number of guests not a positive integer"
     // to check for integer: test at the console Number.isInteger(3.14)
     // TODO if the argument is a valid number of guests, store it in this.numberOfGuests
@@ -30,11 +34,15 @@ class DinnerModel {
   addToMenu(dishToAdd) {
     // array spread syntax example. Make sure you understand the code below.
     // It sets this.dishes to a new array [   ] where we spread (...) the previous value
+    for(let i = 0; i < this.dishes.length; i++) {
+      if(this.dishes[i].id === dishToAdd.id)
+         return;
+    }
     this.dishes = [...this.dishes, dishToAdd];
+    this.notifyObservers({addDish: dishToAdd});
   }
 
   removeFromMenu(dishToRemove) {
-    // callback exercise! Also return keyword exercise
     function hasSameIdCB(dish) {
       if (dish.id != dishToRemove.id) return true;
 
@@ -43,8 +51,15 @@ class DinnerModel {
       // This will keep the dish when we filter below.
       // That is, we will not keep the dish that has the same id as dishToRemove (if any)
     }
+    let isDishInMenu = false;
+    for(let i = 0; i < this.dishes.length; i++) {
+        if(this.dishes[i].id === dishToRemove.id)
+              isDishInMenu = true;
+    }
+    if(!isDishInMenu)
+       return;
     this.dishes = this.dishes.filter(hasSameIdCB);
-    // the test "can remove dishes" should pass
+    this.notifyObservers({removeDish: dishToRemove});
   }
   /* 
        ID of dish currently checked by the user.
@@ -53,11 +68,16 @@ class DinnerModel {
        So we store also abstract data that will influence the application status.
      */
   setCurrentDish(id) {
-    if(id !== undefined && id !== this.currentDish)
-      resolvePromise(getDishDetails(id),this.currentDishPromiseState);
-    this.currentDish = id;
-  }
 
+    if(id !== undefined && id !== this.currentDish)
+    resolvePromise(getDishDetails(id),this.currentDishPromiseState, this.notifyObservers.bind(this));
+
+    if(this.currentDish !== id) {
+      this.currentDish = id;
+      this.notifyObservers({currDish: id})
+    }
+
+  }
   setSearchQuery(q) {
     this.searchParams.query = q;
   }
@@ -65,13 +85,22 @@ class DinnerModel {
   setSearchType(t) {
     this.searchParams.type = t;
   }
-
   doSearch(params) {
+      resolvePromise(searchDishes(params),this.searchResultsPromiseState, this.notifyObservers.bind(this));
+  }
+  addObserver(callback) {
+    this.observers = [...this.observers, callback];
+  }
+  removeObserver(callback) {
+    function removeObserverFromObserversArray(observer) {
+      return observer !== callback
+    }
+    this.observers = this.observers.filter(removeObserverFromObserversArray)
+  }
 
-      resolvePromise(searchDishes(params),this.searchResultsPromiseState);
+  notifyObservers(payload) {
+    try{this.observers.forEach(function invokeObserverCB(obs){obs(payload);})}catch(err){console.error(err); } 
   }
-  doSearch() {
-      resolvePromise(searchDishes(this.searchParams),this.searchResultsPromiseState);
-  }
+
 }
 export default DinnerModel;
--- src/resolvePromise.js
+++ src/resolvePromise.js
@@ -1,16 +1,20 @@
-function resolvePromise(promise, promiseState){
+function resolvePromise(promise, promiseState, notifyObservers){
     promiseState.promise = promise;
     promiseState.data = null;         
     promiseState.error = null;
+
+    if(notifyObservers) notifyObservers();
     function saveDataACB(result){ 
         if(promiseState.promise !== promise) return;
         /* TODO save result in promiseState, as before */
         promiseState.data = result;
+        notifyObservers();
     } 
     function saveErrorACB(err)  { 
         /* TODO same check as above */
         if(promiseState.promise !== promise) return;
         promiseState.error = err;
+        notifyObservers();
         /* TODO save err in promiseState, as before */
     }
     if(promise == null) return;
