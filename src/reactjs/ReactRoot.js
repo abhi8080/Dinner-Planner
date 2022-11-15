10a3b12228c1aa066998f10dff5f9cd53ff4e73a
Abhinav Kalra <kalra@kth.se>
2022-11-15 17:24:24 +0100

    App is completed

--- src/reactjs/ReactRoot.js
+++ src/reactjs/ReactRoot.js
@@ -1,6 +1,33 @@
-// Add relevant imports here 
-
+// Add relevant imports here
+import {
+  firebaseModelPromise,
+  updateFirebaseFromModel,
+  updateModelFromFirebase,
+} from "../firebaseModel.js";
+import resolvePromise from "../resolvePromise.js";
+import promiseNoData from "../views/promiseNoData.js";
+import App from "../views/app.js";
 // Define the ReactRoot component
-// function ReactRoot()
+function ReactRoot() {
+  const [promiseState] = React.useState({});
+  const [, setError] = React.useState({});
+  const [, setData] = React.useState({});
+  function wasCreatedACB() {
+    resolve(firebaseModelPromise());
+  }
+  function resolve(thePromise) {
+    function promiseStateChangedACB() {
+      setError(promiseState["error"]);
+      setData(promiseState["data"]);
+      if (promiseState.data) {
+        updateFirebaseFromModel(promiseState.data);
+        updateModelFromFirebase(promiseState.data);
+      }
+    }
+    resolvePromise(thePromise, promiseState, promiseStateChangedACB);
+  }
+  React.useEffect(wasCreatedACB, []);
+  return promiseNoData(promiseState) || <App model={promiseState.data} />;
+}
 
-// Export the ReactRoot component
+export default ReactRoot;
--- src/reactjs/index.js
+++ src/reactjs/index.js
@@ -1,19 +1,20 @@
 // this is a stub for TW3.5 bootstraping. It helps with a few lab specifics
 import React from "react";
-import {render} from "react-dom";
+import { render } from "react-dom";
 
 // needed for View JSX. In a React project you can use a React import in each View instead
-window.React= React;
+window.React = React;
 
 import firebase from "firebase/app";
 import "firebase/database";
 
 // needed for plugging in a "mock" firebase for testing. In the project simply import firebase where needed, as above
-window.firebase=firebase;
+window.firebase = firebase;
 
 // TODO import navigation
 
 // using require() instead of import, for the above assignments to take effect before ReactRoot is loaded
-const Reactoot=require("/src/reactjs/ReactRoot.js").default;
+const ReactRoot = require("/src/reactjs/ReactRoot.js").default;
 
 // TODO render ReactRoot into the "root" div
+render(<ReactRoot />, document.getElementById("root"));
